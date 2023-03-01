7d5c7a7ef947e60035af46ddcf610bd4b625679c
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-01 23:47:05 +0530

    Created  fields for the class Music

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/domain/Music.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/domain/Music.java
@@ -1,5 +1,14 @@
 package com.niit.bej.c2_sp3_NoSql_Mc1.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Music {
+    @Id
+    private int trackId;
+    private String trackName;
+    private double trackRating;
+    private Artist artist;
 
 }
