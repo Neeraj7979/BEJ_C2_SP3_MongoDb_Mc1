49bb8f97eaddd0d7066169a4ef230551a77778d0
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-01 23:40:45 +0530

    Initial Commit

---
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/MusicApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c2_sp3_NoSql_Mc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/bej/c2_sp3_NoSql_Mc1/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c2_sp3_NoSql_Mc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
