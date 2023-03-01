f32a13bcdbbb19b8f8e4b257415a253a2b406208
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 00:14:38 +0530

    Created Service Interface TrackService and added method to delete the track;

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackService.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackService.java
@@ -2,10 +2,13 @@ package com.niit.bej.c2_sp3_NoSql_Mc1.service;
 
 import com.niit.bej.c2_sp3_NoSql_Mc1.domain.Track;
 import com.niit.bej.c2_sp3_NoSql_Mc1.exception.TrackAlreadyExistException;
+import com.niit.bej.c2_sp3_NoSql_Mc1.exception.TrackNotFoundException;
 
 
 public interface TrackService {
     Track saveTrack(Track track) throws TrackAlreadyExistException;
+
+    boolean deleteTrackById(Integer id) throws TrackNotFoundException;
 
 
 }
