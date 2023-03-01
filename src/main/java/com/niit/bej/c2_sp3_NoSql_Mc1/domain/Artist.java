f14eafea66ef51dc3432de714a616a2924949ee2
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-01 23:54:56 +0530

    Created to string methodfor the field of the class artist

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/domain/Artist.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/domain/Artist.java
@@ -42,4 +42,13 @@ public class Artist {
     public void setArtistName(String artistName) {
         this.artistName = artistName;
     }
+
+    @Override
+    public String
+    toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", artistName='" + artistName + '\'' +
+                '}';
+    }
 }
