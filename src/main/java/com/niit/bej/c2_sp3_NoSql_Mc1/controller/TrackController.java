f3544a4e3a8abcaebc519fa3c46459cb829631d9
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 00:53:06 +0530

    Created class TrackController and added TrackService dependency injection

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/controller/TrackController.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/controller/TrackController.java
@@ -1,7 +1,13 @@
 package com.niit.bej.c2_sp3_NoSql_Mc1.controller;
 
+import com.niit.bej.c2_sp3_NoSql_Mc1.domain.Track;
+import com.niit.bej.c2_sp3_NoSql_Mc1.exception.TrackAlreadyExistException;
 import com.niit.bej.c2_sp3_NoSql_Mc1.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +17,15 @@ public class TrackController {
     @Autowired
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
+    }
+
+    @PostMapping("/saveTrack")
+    public ResponseEntity<?> saveTrack(@RequestBody Track track) {
+        try {
+            Track saveNewTrack = trackService.saveTrack(track);
+            return new ResponseEntity<>(trackService, HttpStatus.ACCEPTED);
+        } catch (TrackAlreadyExistException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 }
