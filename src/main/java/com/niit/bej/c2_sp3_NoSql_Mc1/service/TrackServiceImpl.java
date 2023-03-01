5c767914494d97625fd010b5a3a439c57f8de643
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 00:42:24 +0530

    Created method to delete track;

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackServiceImpl.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrackServiceImpl implements TrackService {
@@ -28,7 +29,11 @@ public class TrackServiceImpl implements TrackService {
 
     @Override
     public boolean deleteTrackById(Integer id) throws TrackNotFoundException {
-        return false;
+        Optional<Track> trackOptional = trackRepository.findById(id);
+        if (trackOptional.isPresent())
+            return true;
+        else
+            return false;
     }
 
     @Override
