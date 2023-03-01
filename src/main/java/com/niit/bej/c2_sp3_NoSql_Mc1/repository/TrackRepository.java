a2035dee2a528218f14c66b1fe96bd5d65eb5355
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 00:04:37 +0530

    Created methods in the repository layer and added query for database

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/repository/TrackRepository.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/repository/TrackRepository.java
@@ -2,6 +2,15 @@ package com.niit.bej.c2_sp3_NoSql_Mc1.repository;
 
 import com.niit.bej.c2_sp3_NoSql_Mc1.domain.Track;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface TrackRepository extends MongoRepository<Track, Integer> {
+   @Query("{'trackRating':?0}")
+   List<Track> fetchByRatingGreaterThan(double rating);
+
+   @Query("{'trackArtist.name':?0}")
+   List<Track> fetchByArtistName(String artistName);
+
 }
