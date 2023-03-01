de43896f8fc55d083b6a34e75d7113686986c0a6
Neeraj7979 <Imnsinha77@outlook.com>
2023-03-02 00:20:19 +0530

    Created Service Interface TrackService to find artist based on rating and artist name;

--- src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackService.java
+++ src/main/java/com/niit/bej/c2_sp3_NoSql_Mc1/service/TrackService.java
@@ -4,11 +4,19 @@ import com.niit.bej.c2_sp3_NoSql_Mc1.domain.Track;
 import com.niit.bej.c2_sp3_NoSql_Mc1.exception.TrackAlreadyExistException;
 import com.niit.bej.c2_sp3_NoSql_Mc1.exception.TrackNotFoundException;
 
+import java.util.List;
+
 
 public interface TrackService {
     Track saveTrack(Track track) throws TrackAlreadyExistException;
 
     boolean deleteTrackById(Integer id) throws TrackNotFoundException;
+
+    List<Track> getAllTrack() throws TrackNotFoundException;
+
+    List<Track> getTrackByRatingGreaterThanGivenRating(double rating) throws TrackNotFoundException;
+
+    List<Track> getAllTrackByArtistName(String artistName) throws TrackNotFoundException;
 
 
 }
